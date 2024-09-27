195baa9749617530e533e416b2537c636ccac9c2
josefrancisco17 <josefranciscoantunesleal@gmail.com>
2024-09-27 22:37:43 +0100

    changes in Navbar and Footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
-export default function Home({ children }) {
+export default function Home() {
   return (
-    <div>Home</div>
+    <div className="min-h-screen p-8 flex items-center justify-center">
+      <h1 className="">Home</h1>
+    </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,31 @@
 export default function Header() {
   return (
-    <div className="w-full flex flex-col md:flex-row">
-      <div className="w-full md:w-1/6 flex flex-row px-5 md:px-10 gap-3 py-3 md:py-0 justify-between md:justify-start">
-        <img 
-          src="/img/logo.jpeg"
-          alt="logo"
-          style={{ width: 50, height: 50 }}
-        />
+    <nav className="w-full flex flex-row fixed font-bold">
+      <div className="w-full flex-row px-5 gap-3 py-3 justify-between">
         <span className="self-center text-lg md:text-xl">NEETI</span>
       </div>
-      <div className="w-full md:w-5/6">
-        <ul className="w-full h-full flex flex-col md:flex-row justify-center md:justify-end py-3 md:py-0">
-          <li className="px-5 py-2 md:px-8 md:py-0 self-center">
+      <div className="w-full">
+        <ul className="w-full h-full flex flex-row justify-end">
+          <li className="px-5 py-2 self-center">
             <a href="/">Home</a>
           </li>
-          <li className="px-5 py-2 md:px-8 md:py-0 self-center">
+          <li className="px-5 py-2 self-center">
             <a href="/">About</a>
           </li>
-          <li className="px-5 py-2 md:px-8 md:py-0 self-center">
+          <li className="px-5 py-2 self-center">
             <a href="/">Contact</a>
           </li>
-          <li className="px-5 py-2 md:px-8 md:py-0 self-center">
+          <li className="px-5 py-2 self-center">
             <a href="/">Repo</a>
           </li>
-          <li className="px-5 py-2 md:px-8 md:py-0 self-center">
+          <li className="px-5 py-2 self-center">
             <a href="/">Team</a>
           </li>
-          <li className="px-5 py-2 md:px-8 md:py-0 self-center">
+          <li className="px-5 py-2 self-center">
             <a href="/">Events</a>
           </li>
         </ul>
       </div>
-    </div>
+    </nav>
   );
 }
