99c265cbd603cbec86bb16dab0134f2b1dfed513
josefrancisco17 <josefranciscoantunesleal@gmail.com>
2024-10-04 16:32:58 +0100

    final version of first deployment

--- src/components/ui/direction-aware-hover.jsx
+++ src/components/ui/direction-aware-hover.jsx
@@ -58,7 +58,7 @@ export const DirectionAwareHover = ({
       onMouseEnter={handleMouseEnter}
       ref={ref}
       className={cn(
-        "md:h-96 w-60 h-60 md:w-96 bg-transparent rounded-lg overflow-hidden group/card relative",
+        "md:h-[500px] w-60 h-60 md:w-[500px] bg-transparent rounded-lg overflow-hidden group/card relative",
         className
       )}>
       <AnimatePresence mode="wait">
