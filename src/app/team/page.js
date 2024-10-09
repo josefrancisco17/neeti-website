096f8fe7fa926552d571cd18be37e41987c0ca1c
josefrancisco17 <josefranciscoantunesleal@gmail.com>
2024-10-09 21:46:42 +0100

    Changes in Navbar and team page code optimizations

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -7,7 +7,7 @@ import { DirectionAwareHover } from "../../components/ui/direction-aware-hover";
 import MemberCard from "@/components/MemberCard";
 
 export default function Team() {
-  const [isUnderConstruction] = useState(false);
+  const [isUnderConstruction] = useState(true);
 
   if (isUnderConstruction) {
     return (
@@ -20,17 +20,49 @@ export default function Team() {
     );
   }
   return (
-    <div className="min-h-screen p-8 flex flex-col items-center justify-center bg-black relative text-white">
+    <div className="z-0 min-h-screen p-8 flex flex-col items-center justify-center bg-black relative text-white">
       <BackgroundBeams className="absolute inset-0 z-0" />
-      <div className="z-10 mt-20 text-white flex flex-col items-center">
+      <div className="z-10 my-20 text-white flex flex-col items-center">
         <h1 className="text-6xl font-bold mb-4">Equipa</h1>
-        <div className="grid grid-cols-4 py-13 px-8 gap-4">
+        <div className="my-10">
+          <div className="w-full h-full flex flex-row gap-4 bg-zinc-800/50 rounded-lg">
+            <a className="p-3">2020/21</a>
+            <a className="p-3">2021/22</a>
+            <a className="p-3">2022/23</a>
+            <a className="p-3">2023/24</a>
+            <a className="p-3">2024/25</a>
+          </div>
+        </div>
+        <h1 className="text-3xl font-bold text-center py-8 text-gray-50">
+          Presidência
+        </h1>
+        <div className="flex flex-row py-6 px-8 gap-6 justify-items-center">
           <MemberCard name="test name" role="test role" />
           <MemberCard name="test name" role="test role" />
           <MemberCard name="test name" role="test role" />
           <MemberCard name="test name" role="test role" />
+        </div>
+
+        <h1 className="text-3xl font-bold text-center py-8 text-gray-50">
+          Departamento de Informática
+        </h1>
+        <div className="flex flex-row py-6 px-8 gap-6 justify-items-center">
           <MemberCard name="test name" role="test role" />
           <MemberCard name="test name" role="test role" />
+        </div>
+
+        <h1 className="text-3xl font-bold text-center py-8 text-gray-50">
+          Departamento de Gestão
+        </h1>
+        <div className="flex flex-row py-6 px-8 gap-6 justify-items-center">
+          <MemberCard name="test name" role="test role" />
+          <MemberCard name="test name" role="test role" />
+        </div>
+
+        <h1 className="text-3xl font-bold text-center py-8 text-gray-50">
+          Departamento de Marketing
+        </h1>
+        <div className="flex flex-row py-6 px-8 gap-6 justify-items-center">
           <MemberCard name="test name" role="test role" />
           <MemberCard name="test name" role="test role" />
         </div>
--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -1,12 +1,12 @@
 export default function MemberCard(props) {
   return (
-    <a className="flex flex-col items-center p-2 rounded-xl bg-white/20">
+    <a className="flex flex-col items-center p-3 w-full max-w-xs h-[350px] bg-white/20 rounded-xl shadow-lg hover:shadow-xl transition-all duration-300">
       <img
-        className="h-full w-full object-cover rounded-xl shadow-xl          "
+        className="min-h-60 min-w-60 object-cover rounded-xl shadow-xl"
         alt="profile picture"
         src="/img/profile.png"
       />
-      <div className="w-full mt-4 flex flex-row justify-between items-center">
+      <div className="mt-4 flex flex-row justify-between items-center">
         <div className="flex flex-col">
           <h3 className="text-xl font-bold text-gray-50">{props.name}</h3>
           <h4 className="text-md font-medium text-gray-200">{props.role}</h4>
